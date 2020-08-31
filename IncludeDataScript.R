d591c4a26ad657b24dcfc393aca2d65fb3007af2
PABLO PALENCIA MAYORDOMO <Pablo.Palencia@uclm.es>
2020-08-31 14:07:12 +0200

    Include data in the package

--- IncludeDataScript.R
+++ IncludeDataScript.R
@@ -1,4 +1,3 @@
 
-
 RedDeerdata <- read.table("D:/rprojects/trappingmotion_Rpackage/trappingmotion/data/RedDeerdata.txt", sep = ";", dec=".", header=TRUE, as.is=TRUE)
 save(RedDeerdata, file="data/RedDeerdata.RData")

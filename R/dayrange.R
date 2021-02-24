398a9ee342b3ebe09c0627ec5efcba4a907d73e4
PabloPalencia <pablo.palencia@uclm.es>
2021-02-24 11:47:40 +0100

    updating dayrange function to include in the variance estimation the uncertainty associated to the proportion spent on each behaviour. See comments from reviewer1 in the second revision

--- R/dayrange.R
+++ R/dayrange.R
@@ -1,4 +1,3 @@
-
 dayrange <- function(act, act_se, speed_data){
 
   if(any(speed_data[,3] < 0 | (speed_data[,2]-speed_data[,1]) > 0 | act > 1 | act < 0)){
@@ -6,21 +5,32 @@ dayrange <- function(act, act_se, speed_data){
   }else{
 
     speed_data <- speed_data[order(speed_data[,1], decreasing = T), ]
-    n_T <- 0
-    for(i in 1:length(speed_data[,1])){ #Este loop es para calcular el "denominador"
+    #n_T <- 0
 
-      n_t <- speed_data[i,3]*(speed_data[1,1]/speed_data[i,1])
-      n_T <- sum(n_T, n_t)
+    for(i in 1:length(speed_data[,1])){
 
-    }
+      speed_data[i,4] <- speed_data[i,3]*(speed_data[1,1]/speed_data[i,1])
 
-    Dr <- 0; ses<-c()
-    for(i in 1:length(speed_data[,1])){ #Este loop es para calcular el DR en m
+    }; names(speed_data)[4] <- "m"
 
-      dr <- (speed_data[i,1]*act*(speed_data[1,1]/speed_data[i,1])*speed_data[i,3])/n_T
-      Dr <- sum(Dr, dr)
-      se <- dr*sqrt((act_se/act)^2+(speed_data[i,2]/speed_data[i,1])^2+((act_se/act)^2*(speed_data[i,2]/speed_data[i,1])^2))
-      ses <- c(ses,se)
+    for(i in 1:length(speed_data[,1])){
+
+      speed_data[i,5] <- speed_data[i,4]/sum(speed_data[,4])
+
+    }; names(speed_data)[5] <- "p"
+
+
+    Dr <- 0; ses<-0
+    for(i in 1:length(speed_data[,1])){
+
+      sepi <- sqrt((speed_data[i,5]*(1-speed_data[i,5]))/(sum(speed_data[,4])-1))
+      ai <- act*speed_data[i,5]
+      seai <- ai*sqrt((sepi/speed_data[i,5])^2+(act_se/act)^2+((act_se/act)^2*(speed_data[i,2]/speed_data[i,1])^2))
+      dri <- speed_data[i,1]*ai
+      sedri <- dri*sqrt((speed_data[i,2]/speed_data[i,1])^2+(seai/ai)^2+((speed_data[i,2]/speed_data[i,1])^2*(seai/ai)^2))
+
+      Dr <- sum(Dr, dri)
+      ses <- c(ses,sedri)
 
       if(i==length(speed_data[,1])){
         DR <<- Dr*86.4; SES<- 0

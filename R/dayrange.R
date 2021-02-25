b030142fabb71e694440ae908ff7bc0e6b54447d
PabloPalencia <pablo.palencia@uclm.es>
2021-02-25 09:42:39 +0100

    update function dayrange

--- R/dayrange.R
+++ R/dayrange.R
@@ -5,7 +5,6 @@ dayrange <- function(act, act_se, speed_data){
   }else{
 
     speed_data <- speed_data[order(speed_data[,1], decreasing = T), ]
-    #n_T <- 0
 
     for(i in 1:length(speed_data[,1])){
 
@@ -20,20 +19,20 @@ dayrange <- function(act, act_se, speed_data){
     }; names(speed_data)[5] <- "p"
 
 
-    Dr <- 0; ses<-0
+    Dr <- c(); ses<-c()
     for(i in 1:length(speed_data[,1])){
 
       sepi <- sqrt((speed_data[i,5]*(1-speed_data[i,5]))/(sum(speed_data[,4])-1))
       ai <- act*speed_data[i,5]
-      seai <- ai*sqrt((sepi/speed_data[i,5])^2+(act_se/act)^2+((act_se/act)^2*(speed_data[i,2]/speed_data[i,1])^2))
+      seai <- ai*sqrt((sepi/speed_data[i,5])^2+(act_se/act)^2+((act_se/act)^2*(sepi/speed_data[i,5])^2))
       dri <- speed_data[i,1]*ai
       sedri <- dri*sqrt((speed_data[i,2]/speed_data[i,1])^2+(seai/ai)^2+((speed_data[i,2]/speed_data[i,1])^2*(seai/ai)^2))
 
       Dr <- sum(Dr, dri)
-      ses <- c(ses,sedri)
+      ses <- c(ses, sedri)
 
       if(i==length(speed_data[,1])){
-        DR <<- Dr*86.4; SES<- 0
+        DR <<- Dr*86.4; SES<- c()
         for(i in 1:length(speed_data[,1])){
 
           SE<- ses[i]^2
